11092376a83303d6894909c07b1861abeb32e1d9
daafith <davidbaak@gmail.com>
2020-09-29 12:30:22 +0200

    added some effects to the image

--- showImage.py
+++ showImage.py
@@ -1,5 +1,13 @@
 import cv2
+import util
 
 img = cv2.imread("Resources/Images/david.jpg")
-cv2.imshow("David", img)
-cv2.waitKey(0)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+blurred = cv2.GaussianBlur(gray, (9, 9), 0)
+canny = cv2.Canny(img, 80, 80)
+
+while not util.is_exit_key_pressed():
+    cv2.imshow("David", img)
+    cv2.imshow("Gray David", gray)
+    cv2.imshow("Blurred Gray David", blurred)
+    cv2.imshow("Canny David", canny)
