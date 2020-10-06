3496bdf435bd97b75c2348343666e4d6f05f7a61
daafith <davidbaak@gmail.com>
2020-10-06 16:23:41 +0200

    Color detection

--- detectColor.py
+++ detectColor.py
@@ -1,24 +1,47 @@
 import cv2
 import util
+import numpy as np
 
 
 def noop(f):
     pass
 
 
-car = cv2.imread("Resources/Images/car.jpg")
-window_name = "Foobar"
-cv2.namedWindow(window_name)
-cv2.resizeWindow(window_name, 700, 400)
-cv2.createTrackbar("Hue min", window_name, 0, 179, noop)
-cv2.createTrackbar("Hue max", window_name, 179, 179, noop)
-cv2.createTrackbar("Sat min", window_name, 0, 255, noop)
-cv2.createTrackbar("Sat max", window_name, 255, 255, noop)
-cv2.createTrackbar("Value min", window_name, 0, 255, noop)
-cv2.createTrackbar("Value max", window_name, 255, 255, noop)
+car = cv2.imread("Resources/Images/car.png")
+car_window = "Foobar"
+trackbar_hue_min = "Hue min"
+trackbar_hue_max = "Hue max"
+trackbar_saturation_min = "Sat min"
+trackbar_saturation_max = "Sat max"
+trackbar_value_min = "Value min"
+trackbar_value_max = "Value max"
 
-hue_saturation = cv2.cvtColor(car, cv2.COLOR_BGR2HSV)
+cv2.namedWindow(car_window)
+# cv2.resizeWindow(car_window, 700, 500)
+
+# Initial values are normally 0, but the ideal values can be found by manually modifying the trackbars
+cv2.createTrackbar(trackbar_hue_min, car_window, 0, 179, noop)
+cv2.createTrackbar(trackbar_hue_max, car_window, 19, 179, noop)
+cv2.createTrackbar(trackbar_saturation_min, car_window, 110, 255, noop)
+cv2.createTrackbar(trackbar_saturation_max, car_window, 240, 255, noop)
+cv2.createTrackbar(trackbar_value_min, car_window, 153, 255, noop)
+cv2.createTrackbar(trackbar_value_max, car_window, 255, 255, noop)
+
+hsv_image = cv2.cvtColor(car, cv2.COLOR_BGR2HSV)
 
 while not util.is_exit_key_pressed():
-    cv2.imshow("Car", car)
-    cv2.imshow("HSV", hue_saturation)
+    hue_min = cv2.getTrackbarPos(trackbar_hue_min, car_window)
+    hue_max = cv2.getTrackbarPos(trackbar_hue_max, car_window)
+    sat_min = cv2.getTrackbarPos(trackbar_saturation_min, car_window)
+    sat_max = cv2.getTrackbarPos(trackbar_saturation_max, car_window)
+    value_min = cv2.getTrackbarPos(trackbar_value_min, car_window)
+    value_max = cv2.getTrackbarPos(trackbar_value_max, car_window)
+
+    lower_bound = np.array([hue_min, sat_min, value_min])
+    upper_bound = np.array([hue_max, sat_max, value_max])
+    mask = cv2.inRange(hsv_image, lower_bound, upper_bound)
+    result = cv2.bitwise_and(car, car, mask=mask)
+
+    cv2.imshow("Combined", util.stack_images(0.7, ([car, hsv_image], [mask, result])))
+
+
--- util.py
+++ util.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 
 def is_exit_key_pressed(delay=1):
@@ -22,3 +23,37 @@ def resize_image(image, percentage):
     divisor = 100 / percentage
     dimension = int(image.shape[1] / divisor), int(image.shape[0] / divisor)
     return cv2.resize(image, dimension)
+
+
+# Copy pasted this from GitHub, may refactor it later
+def stack_images(scale, img_array):
+    rows = len(img_array)
+    cols = len(img_array[0])
+    rows_available = isinstance(img_array[0], list)
+    width = img_array[0][0].shape[1]
+    height = img_array[0][0].shape[0]
+    if rows_available:
+        for x in range(0, rows):
+            for y in range(0, cols):
+                if img_array[x][y].shape[:2] == img_array[0][0].shape[:2]:
+                    img_array[x][y] = cv2.resize(img_array[x][y], (0, 0), None, scale, scale)
+                else:
+                    img_array[x][y] = cv2.resize(img_array[x][y], (img_array[0][0].shape[1], img_array[0][0].shape[0]),
+                                                 None, scale, scale)
+                if len(img_array[x][y].shape) == 2: img_array[x][y] = cv2.cvtColor(img_array[x][y], cv2.COLOR_GRAY2BGR)
+        image_blank = np.zeros((height, width, 3), np.uint8)
+        hor = [image_blank] * rows
+        hor_con = [image_blank] * rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(img_array[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if img_array[x].shape[:2] == img_array[0].shape[:2]:
+                img_array[x] = cv2.resize(img_array[x], (0, 0), None, scale, scale)
+            else:
+                img_array[x] = cv2.resize(img_array[x], (img_array[0].shape[1], img_array[0].shape[0]), None, scale, scale)
+            if len(img_array[x].shape) == 2: img_array[x] = cv2.cvtColor(img_array[x], cv2.COLOR_GRAY2BGR)
+        hor = np.hstack(img_array)
+        ver = hor
+    return ver
